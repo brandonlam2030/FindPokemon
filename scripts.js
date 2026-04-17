e3d340061522b36f9b339d234f17fc12669c5de4
Brandon Lam <brandonlam2030@gmail.com>
2026-04-16 21:39:55 -0700

    Added csv parsing into key-value pairs and adjusted card generation

--- scripts.js
+++ scripts.js
@@ -1,97 +1,99 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
- */
+var pokemon = []
+const frame = [
+  "against_bug",
+  "against_dark",
+  "against_dragon",
+  "against_electric",
+  "against_fairy",
+  "against_fight",
+  "against_fire",
+  "against_flying",
+  "against_ghost",
+  "against_grass",
+  "against_ground",
+  "against_ice",
+  "against_normal",
+  "against_poison",
+  "against_psychic",
+  "against_rock",
+  "against_steel",
+  "against_water",
+  "attack",
+  "base_egg_steps",
+  "base_happiness",
+  "base_total",
+  "capture_rate",
+  "classification",
+  "defense",
+  "experience_growth",
+  "height_m",
+  "hp",
+  "japanese_name",
+  "name",
+  "percentage_male",
+  "pokedex_number",
+  "sp_attack",
+  "sp_defense",
+  "speed",
+  "type1",
+  "type2",
+  "weight_kg",
+  "generation",
+  "is_legendary"]
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+async function loadCSVData() {
+  const response = await fetch("pokemon.csv")
+  const text = await response.text();
 
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
+  let data = text.split("\n");
+  data.forEach((obj) => {
+    obj = obj.slice(obj.search("]")+3);
+    const attributes = (obj.split(","));
 
-// This function adds cards the page to display the data in the array
-function showCards() {
+    let zipped = {}
+    for(let i = 0 ; i < attributes.length; i++) {
+      zipped[frame[i]] = attributes[i];
+    }
+    pokemon.push(zipped);
+  })
+}
+
+
+var offset = 1;
+
+
+async function showCards() {
+  await loadCSVData();
   const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML = "";
+
   const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
+  for (let i = 0; i < 28; i++) {
+    let target = pokemon[offset++];
 
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
 
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
+    editCardContent(nextCard, target.name, target.pokedex_number); // Edit title and image
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 }
 
 function editCardContent(card, newTitle, newImageURL) {
-  card.style.display = "block";
+  card.style.display = "flex";
 
   const cardHeader = card.querySelector("h2");
   cardHeader.textContent = newTitle;
 
   const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
+  cardImage.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${newImageURL.trim()}.png`;
   cardImage.alt = newTitle + " Poster";
 
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
+  
   console.log("new card:", newTitle, "- html: ", card);
 }
 
-// This calls the addCards() function when the page is first loaded
+
 document.addEventListener("DOMContentLoaded", showCards);
 
-function quoteAlert() {
-  console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!",
-  );
-}
 
-function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
-  showCards(); // Call showCards again to refresh
-}
+
